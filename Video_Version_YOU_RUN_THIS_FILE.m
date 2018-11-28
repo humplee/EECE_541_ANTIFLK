87650f8b94d018f9b035baf10d43189ff3faacb5
humplee <littleted1221@163.com>
2018-11-28 09:16:53 -0800

    tif

--- Video_Version_YOU_RUN_THIS_FILE.m
+++ Video_Version_YOU_RUN_THIS_FILE.m
@@ -3,21 +3,15 @@ clear all;
 
 global curvebuffer;
 curvebuffer=zeros(256);
-filename='D:\Content\LG.ts';
+tiffname='D:\Content\img\';
+outVideo='D:\Content\finaltestX.yuv';
 
-outVideo='D:\Content\testsecondround3.yuv';
 
-v = VideoReader(filename),'RGB48';
-counter=0;
-while (hasFrame(v))
-    
-    counter=counter+1;
-    nextFrame = readFrame(v);
-    if counter <86
-        continue;
-    end
-    SDR=ToneMapping(nextFrame,v.BitsPerPixel);
-    
+filenamecounter=1;
+while (true)
+    v=imread(['D:\Content\img\',num2str(filenamecounter),'.tif']);
+    filenamecounter=filenamecounter+1;       
+    SDR=ToneMapping(v,48);
     
     Video_name=outVideo;
     ybr=RGB2YCbCr(SDR,1,'BT.709',false);
@@ -29,7 +23,9 @@ while (hasFrame(v))
     lumch=ybr_10(:,:,1);
 %     lumch = imsharpen(lumch,'Amount',0.8,'Threshold',0);
     WriteFramePlanar(lumch, chr1, chr2, Video_name, 2, 8);         
-    
+    if filenamecounter >23
+        break;
+    end
     
     
 
